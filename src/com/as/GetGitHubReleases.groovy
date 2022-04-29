09e1c287be8a29da256ffe29b261744500b03bc0
Anastasia Shuvalova (DHL IT Services), external <anastasia.shuvalova@dhl.com>
2022-04-29 17:58:07 +0200

    get releases

--- src/com/as/GetGitHubReleases.groovy
+++ src/com/as/GetGitHubReleases.groovy
@@ -8,6 +8,8 @@ class GetGitHubReleases implements java.io.Serializable {
   }
 
   public void getReleases(String repoUrl){
-    this.context.sh("echo Initial commit")
+    def (protocol, hostname, owner, repo_name) = repoUrl.tokenize('/')
+    def curl_command = "curl -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/${owner}/${repo_name}/releases -o releases-${repo_name}.txt"
+    this.context.sh(curl_command)
   }
 }

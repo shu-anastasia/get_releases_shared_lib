b403d570ece610fee536fec711eba33263ce67be
Anastasia Shuvalova (DHL IT Services), external <anastasia.shuvalova@dhl.com>
2022-04-29 18:05:09 +0200

    added input validation

--- src/com/as/GetGitHubReleases.groovy
+++ src/com/as/GetGitHubReleases.groovy
@@ -8,8 +8,20 @@ class GetGitHubReleases implements java.io.Serializable {
   }
 
   public void getReleases(String repoUrl){
-    def (protocol, hostname, owner, repo_name) = repoUrl.tokenize('/')
-    def curl_command = "curl -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/${owner}/${repo_name}/releases -o releases-${repo_name}.txt"
-    this.context.sh(curl_command)
+    def validURL = validateRepoURL(repoUrl)
+    if (validURL){
+        this.context.sh("echo Getting the releases from ${repoUrl}...")
+        def (protocol, hostname, owner, repo_name) = repoUrl.tokenize('/')
+        def curl_command = "curl -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/${owner}/${repo_name}/releases -o releases-${repo_name}.txt"
+        this.context.sh(curl_command)
+    }
+    else{
+        this.context.error("Invalid repository address")
+    }
+  }
+
+  boolean validateRepoURL(String url){
+    def pattern = ~"(?:git@|https://)github.com[:/](.*)"
+    return pattern.matcher(url).matches()
   }
 }
